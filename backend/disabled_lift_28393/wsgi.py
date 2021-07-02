a5d77c1b2b9e33ca65ea6afb571edf24d040fb64
Crowdbotics <team@crowdbotics.com>
2021-07-02 21:19:05 +0000

    Initial commit.

---
+++ backend/disabled_lift_28393/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for disabled_lift_28393 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disabled_lift_28393.settings')
+
+application = get_wsgi_application()
